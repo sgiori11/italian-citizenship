ae2842fd2678fc1c93a077f3a8b790487ccaa6d6
“sgiori11” <stephaniegiori9@gmail.com>
2023-08-05 23:38:52 +0200

    hero

---
+++ src/app/components/HeroSection.js
@@ -0,0 +1,17 @@
+import styles from '../../styles/HeroSection.module.css';
+import Image from 'next/image';
+
+export default function HeroSection() {
+    return (
+    <section className={styles.container}>
+        <h1>Are You Eligible For Italian Citizenship By Descent?</h1>
+         <Image 
+                priority={true}
+           
+                src=""
+                alt="A hand-drawn fern"
+                width={350}
+            />
+    </section>
+  );
+}
