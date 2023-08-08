f6b330932529bc78aef7af17584710dc9cc976a5
“sgiori11” <stephaniegiori9@gmail.com>
2023-08-08 18:37:51 +0200

    built the About section

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,18 +1,35 @@
 import styles from '../../styles/About.module.css';
 import Image from 'next/image'
 import me from '/public/me.png';
+import vlog from '/public/vlog.png';
 
 
 export default function About() {
     return(
         <section className={styles.aboutCont}>
             <h2>About</h2>
-            <Image 
-                width={400}
-                alt='Circular images of Portofino, Toscana, and Venezia.'
-                src={me}
-                className={styles.myImg}
-        />
+            <div className={styles.mainCont}>
+                <div className={styles.imgCont}>
+                <Image 
+                    width={400}
+                    alt='Circular images of Portofino, Toscana, and Venezia.'
+                    src={me}
+                    className={styles.myImg}
+                />
+                <Image 
+                        width={400}
+                        alt='Find me on TikTok @stephanie_in_milan, where I vlog about living in Italy!'
+                        src={vlog}
+                        className={styles.myVlog}
+                /></div>
+                <p>Hi! My name is Stephanie, and I successfully applied for Italian citizenship by descent in 2019. I now work in Milan as a web experimentation specialist, and I built this site to help other people with Italian ancestry determine if they are eligible.
+
+<br /><br />If you’re interested in English-speaking jobs in Italy, check out my other website, <a href="https://startupjobsitalia.com/" target="_blank" rel="noreferrer">startupjobsitalia.com!</a>
+
+
+
+                </p>
+            </div>
 
         </section>
     )
